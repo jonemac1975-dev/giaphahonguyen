02f3d313f579c77c62b6bd8128303b9fba8a25b9
Jone - Mac <jone@example.com>
2026-03-10 19:55:04 +0700

    first commit

---
+++ src/config.js
@@ -0,0 +1,19 @@
+// src/config.js
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAUX-WzwMX_543EUA2M_thKymnjoE9BgIw",
+  authDomain: "ho-nguyen-huynh.firebaseapp.com",
+  databaseURL: "https://ho-nguyen-huynh-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ho-nguyen-huynh",
+  storageBucket: "ho-nguyen-huynh.appspot.com", // ✅ SỬA
+  messagingSenderId: "494535971628",
+  appId: "1:494535971628:web:8801e31365dfa8dcc31289"
+};
+
+// CHẾ ĐỘ (sau này đổi sang host chỉ đổi biến này)
+export const DATA_MODE = "firebase"; 
+// "firebase" | "host"
+
+
+
+
